07b5d914b9898309807ec416dd54c4338d723292
Sandesh Satyal <sandesh@neodigitalnp.com>
2023-11-09 17:11:53 +0545

    Bug fix: review model remade

---
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    public function index()
+    {
+        $reviews = Review::whereActive(1)->paginate();
+        return view('admin.reviews.index',compact('reviews'));
+    }
+
+    public function create(Request $request)
+    {
+        $courses = Course::whereActive(1)->get();
+        return view('admin.reviews.create',compact('courses'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->active ? $request['active'] = 1 : $request['active'] = 0;
+        Review::create($request->all());
+        return redirect()->route('reviews.index')->with('success','Review added');
+    }
+
+    public function edit(Review $review, Request $request)
+    {
+        $courses = Course::whereActive(1)->get();
+        return view('admin.reviews.edit',compact('review','courses'));
+    }
+
+    public function update(Review $review, Request $request)
+    {
+        $request->active ? $request['active'] = 1 : $request['active'] = 0;
+        $review->update($request->all());
+        return redirect()->route('reviews.index')->with('success','Review updated');
+    }
+
+    public function destroy(Review $review)
+    {
+        $review->delete();
+        return redirect()->route('reviews.index')->with('success','Review deleted');
+    }
+}
