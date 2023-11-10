f89d004b52097d6d98018eebe65a2aba189c5d7b
Sandesh Satyal <sandesh@neodigitalnp.com>
2023-11-10 16:53:20 +0545

    Seeders added in main seeder queue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CategoryTableSeeder::class);
+        $this->call(RolesPermissionsSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
